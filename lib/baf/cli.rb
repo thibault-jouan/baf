56938cb65b44dff10ad1090c2fdca0745f9f33d8
Thibault Jouan <tj@a13.fr>
2015-11-29 01:23:09 +0000

    Implement program argument handling

--- lib/baf/cli.rb
+++ lib/baf/cli.rb
@@ -3,13 +3,19 @@ module Baf
     EX_SOFTWARE = 70
 
     class << self
-      def run _, stderr: $stderr
-        new.run!
+      def run arguments, stderr: $stderr
+        new(arguments).run!
       rescue StandardError => e
         stderr.puts "#{e.class.name}: #{e.message}"
         stderr.puts e.backtrace.map { |l| '  %s' % l }
         exit EX_SOFTWARE
       end
+    end
+
+    attr_reader :arguments
+
+    def initialize arguments
+      @arguments = arguments
     end
 
     def run!
