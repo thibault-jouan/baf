352f67069d4f10fdb44361171dcf12fba1120548
Thibault Jouan <tj@a13.fr>
2016-02-07 11:22:22 +0000

    Add missing unit tests for CLI class

--- spec/baf/cli_spec.rb
+++ spec/baf/cli_spec.rb
@@ -29,6 +29,13 @@ module Baf
         trap_exit! { run }
       end
 
+      it 'parses CLI arguments' do
+        cli
+        allow(described_class).to receive(:new) { cli }
+        expect(cli).to receive :parse_arguments!
+        run
+      end
+
       it 'runs the CLI' do
         cli
         allow(described_class).to receive(:new) { cli }
@@ -64,6 +71,13 @@ module Baf
     describe '#env' do
       it 'returns given env' do
         expect(cli.env).to eq env
+      end
+    end
+
+    describe '#parse_arguments!' do
+      it 'asks the option parser to parse CLI arguments' do
+        expect(options_parser).to receive(:parse!).with arguments
+        cli.parse_arguments!
       end
     end
   end
