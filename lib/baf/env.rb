c1bec470885c3e1ce3478fc1d9e7c12d37c8fb7f
Thibault Jouan <tj@a13.fr>
2016-03-10 21:01:25 +0000

    Implement `Env#puts'

--- lib/baf/env.rb
+++ lib/baf/env.rb
@@ -3,7 +3,7 @@ require 'forwardable'
 module Baf
   class Env
     extend Forwardable
-    def_delegator :@output, :print
+    def_delegators :@output, :print, :puts
 
     def initialize output
       @output = output
