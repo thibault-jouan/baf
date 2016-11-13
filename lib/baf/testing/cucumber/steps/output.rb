4adef22c82f9a4f9c33fdecf7c0cca3e1ca281a5
Thibault Jouan <tj@a13.fr>
2016-11-13 07:07:14 +0000

    Add new output steps in cucumber UAT API

--- lib/baf/testing/cucumber/steps/output.rb
+++ lib/baf/testing/cucumber/steps/output.rb
@@ -8,8 +8,9 @@ def build_regexp pattern, options
   end)
 end
 
-def expect_output content
-  expect(last_command_started.output).to eq unescape_text content
+def expect_output content, stream: :output
+  stream = :stderr if stream == :error
+  expect(last_command_started.send stream).to eq unescape_text content
 end
 
 
@@ -17,12 +18,17 @@ Then /^the output must contain "([^"]+)"$/ do |content|
   expect(last_command_started.output).to include unescape_text content
 end
 
+Then /^the output must not contain "([^"]+)"$/ do |content|
+  expect(last_command_started.output).not_to include unescape_text content
+end
+
 Then /^the output must contain exactly:$/ do |content|
   expect_output content + $/
 end
 
-Then /^the output must contain exactly "([^"]+)"$/ do |content|
-  expect_output content
+Then /^the( error)? output must contain exactly "([^"]+)"$/ do |stream, content|
+  stream = stream ? :error : :output
+  expect_output content, stream: stream
 end
 
 Then /^the output must match \/([^\/]+)\/([a-z]*)$/ do |pattern, options|
