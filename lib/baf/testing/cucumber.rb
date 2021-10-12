82c41d584c833bd66b6ac4422c1c66239a4f7cc5
Thibault Jouan <tj@a13.fr>
2021-10-12 13:33:27 +0000

    Support Ruby 3.0

--- lib/baf/testing/cucumber.rb
+++ lib/baf/testing/cucumber.rb
@@ -1,5 +1,7 @@
 require 'aruba/api'
 require 'aruba/cucumber/hooks'
 
+require 'baf/testing/aruba/ruby30_compat'
+
 require 'baf/testing/cucumber/steps/execution'
 require 'baf/testing/cucumber/steps/output'
